a9607c61459b1ba9fe191a02c692bd79485fc638
AJ <ajautz@gmail.com>
2013-03-30 12:34:06 -0400

    Initial commit, Courso

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Courso::Application.config.secret_token = 'e23d201cb51f6e01d242b020ad865b145338908420411d4537b97a822fe2fcf88229e34dd76b9c50fcab1dd915d5a4e43071ea90446e6dc3799645f4c6a68c80'
